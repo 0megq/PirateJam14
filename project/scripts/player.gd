8ce3f18bc777623eb94ac77763abec9e16679d65
0megq <nvatanshenas55@gmail.com>
2024-01-14 10:50:22 -0800

    Fixed player acceleration
    - Player acceleration now uses delta to be frame independent

--- project/scripts/player.gd
+++ project/scripts/player.gd
@@ -2,12 +2,12 @@ class_name Player
 extends CharacterBody2D
 #Speed & Acceleration may need to be tweaked based on other gameplay elements.
 const MAX_SPEED: float = 600.0
-const ACCELERATION: float = 100.0 
+const ACCELERATION: float = 3000.0  # A MAX_SPEED / ACCELERATION = how much time (in seconds) to reach max speed. t = v/a
 
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("Left","Right","Up","Down")
-	velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION)
+	velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
 	#Removes speed-down before speeding back up. Satisfying :D
 	if direction == Vector2(0,0):
 		velocity = Vector2(0,0)
