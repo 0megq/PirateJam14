44f414fe7990a06b34262d86121ba957e155085b
Danooget <154860455+Danooget@users.noreply.github.com>
2024-01-17 18:04:22 -0600

    Controller support & aiming cursor
    Added controller support boolean joypad.
    
    Added 4 more inputs for the right joystick.
    
    Restructured animations to change on the fly if joypad joystick/keyboard mouse input is detected.
    
    added more idle directions to animation tree.
    
    Deleted unnecessary sprite assets.
    
    Unnecessarily changed the names of a few variables and didn't change them back (sorry)

--- project/scripts/player.gd
+++ project/scripts/player.gd
@@ -19,11 +19,12 @@ var jam_scene = preload("res://scenes/jam.tscn")
 var is_hurt: bool = false
 
 var can_fire: bool = true
-var fire_input: bool = false
-var aim_input: bool = false
+var is_firing: bool = false
+var is_aiming: bool = false
 
 var dir_input: Vector2
-var max_distance: int = 20
+var js_r_input: Vector2
+var joypad: bool
 
 var current_health: int :
 	set(value):
@@ -54,24 +55,40 @@ func _physics_process(delta: float) -> void:
 # Gets input and stores in the appropriate input variables
 func input() -> void:
 	dir_input = Input.get_vector("left", "right", "up", "down")
-	fire_input = Input.is_action_pressed("fire")
-	aim_input = Input.is_action_pressed("aim")
-
+	is_firing = Input.is_action_pressed("fire")
+	is_aiming = Input.is_action_pressed("aim")
+	js_r_input = Input.get_vector("aim_left","aim_right","aim_up","aim_down")
 
 # Animates player based off input
 func animate() -> void:
 	var mouse_position = get_global_mouse_position()
 	var mouse_dir := global_position.direction_to(mouse_position)
-	var joystick_r_dir := Vector2(Input.get_joy_axis(0, JOY_AXIS_RIGHT_X), Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y)).normalized()
-	$Cursor.rotation = mouse_dir.angle()
-	$Cursor.position = clamp(mouse_position, mouse_dir * 20, mouse_dir * 20)
-	
-	if dir_input == Vector2.ZERO:
-		$AnimationTree.get("parameters/playback").travel("Idle")
-		$AnimationTree.set("parameters/Idle/blend_position", mouse_dir)
+	var js_r_position = js_r_input.normalized()
+	#Controller
+	if joypad == true:
+		if js_r_input != Vector2.ZERO:
+			var aim_dir = clamp(js_r_position, js_r_position * 20, js_r_position * 20)
+			$Cursor.rotation = js_r_input.angle()
+			$Cursor.position = aim_dir
+		else:
+			pass
+
+		if dir_input == Vector2.ZERO:
+			$AnimationTree.get("parameters/playback").travel("Idle")
+			$AnimationTree.set("parameters/Idle/blend_position", $Cursor.position)
+		else:
+			$AnimationTree.get("parameters/playback").travel("Walking")
+			$AnimationTree.set("parameters/Walking/blend_position", dir_input)
+	#Mouse & Keyboard
 	else:
-		$AnimationTree.get("parameters/playback").travel("Walking")
-		$AnimationTree.set("parameters/Walking/blend_position", dir_input)
+		$Cursor.rotation = mouse_dir.angle()
+		$Cursor.position = clamp(mouse_position, mouse_dir * 20, mouse_dir * 20)
+		if dir_input == Vector2.ZERO:
+			$AnimationTree.get("parameters/playback").travel("Idle")
+			$AnimationTree.set("parameters/Idle/blend_position", mouse_dir)
+		else:
+			$AnimationTree.get("parameters/playback").travel("Walking")
+			$AnimationTree.set("parameters/Walking/blend_position", dir_input)
 
 
 # Moves the player (duh)
@@ -89,7 +106,7 @@ func move(delta: float) -> void:
 # Manages jam firing. 
 func manage_firing() -> void:	
 	# Firing - modify if/else statements once regular attack is ready.
-	if fire_input and aim_input and can_fire:
+	if is_firing and is_aiming and can_fire:
 		can_fire = false
 		fire()
 		fire_timer.start()
@@ -143,12 +160,12 @@ func take_damage(dmg: int) -> void:
 func die() -> void:
 	current_health = max_health #PLACEHOLDER!!!! Remove once death mechanic is finished.
 
-#Debug
+
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("ui_accept"):
-		take_damage(3)
-		#print(particle_count)
-#/debug
+	if(event is InputEventJoypadButton) or (event is InputEventJoypadMotion):
+		joypad = true
+	elif(event is InputEventKey) or (event is InputEventMouseMotion):
+		joypad = false
 
 
 
