1314f8e0f2e4c9ea952c095576f70bc07949ad8f
Danooget <154860455+Danooget@users.noreply.github.com>
2024-01-18 20:38:33 -0600

    Updated tilemap spread
    Updated code in tilemap spread
    Added framework for sounds
    Added placeholder(?) footstep sounds

--- project/scripts/player.gd
+++ project/scripts/player.gd
@@ -48,7 +48,7 @@ func _physics_process(delta: float) -> void:
 	input()
 	
 	animate()
-	
+	sounds()
 	move(delta)
 	manage_firing()
 
@@ -90,6 +90,13 @@ func animate() -> void:
 			$AnimationTree.get("parameters/playback").travel("Walking")
 			$AnimationTree.set("parameters/Walking/blend_position", dir_input)
 
+
+func sounds():
+	if velocity.length() > 0:
+		if $Footsteps.time_left == 0:
+			$AudioStreamPlayer2D.set_pitch_scale(randf_range(0.8, 1.2))
+			$AudioStreamPlayer2D.play()
+			$Footsteps.start()
 
 # Moves the player (duh)
 func move(delta: float) -> void:
